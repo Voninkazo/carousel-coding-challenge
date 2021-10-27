169760ee3240f267a263f038ff7cdfa19d54b08a
sandy <sandy.rav@onja.org>
2021-10-27 15:15:51 +0300

    Add multiple screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px',
+    },
     extend: {
       boxShadow: {
         contentCardShadow: '0px 8px 20px rgba(0, 0, 0, 0.15)',

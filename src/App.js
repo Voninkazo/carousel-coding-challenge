a5f62efeba239d46a4370dc411b3f137e36ab96d
sandy <sandy.rav@onja.org>
2021-10-27 15:14:14 +0300

    Fix constant naming variables

--- src/App.js
+++ src/App.js
@@ -6,42 +6,51 @@ import { Loading } from './components/Loading'
 
 import { getCarouselData } from './data-fetch-helper'
 
-const MAX_IMAGES = 4
+const MAX_DISPLAYED_BLOCKS = 4
+
 function App() {
   const [sliceStartIndex, setSliceStartIndex] = useState(0)
   const [allBlocksData, setAllBlocksData] = useState([])
   const [isLoading, setIsLoading] = useState(true)
 
   const [carouselData, setCarouselData] = useState(
-    allBlocksData.slice(sliceStartIndex, sliceStartIndex + MAX_IMAGES)
+    allBlocksData.slice(sliceStartIndex, sliceStartIndex + MAX_DISPLAYED_BLOCKS)
   )
 
   function slideToLeft() {
-    setSliceStartIndex(sliceStartIndex - MAX_IMAGES)
+    setSliceStartIndex(sliceStartIndex - MAX_DISPLAYED_BLOCKS)
   }
 
   function slideToRight() {
-    setSliceStartIndex(sliceStartIndex + MAX_IMAGES)
+    setSliceStartIndex(sliceStartIndex + MAX_DISPLAYED_BLOCKS)
   }
 
   // Get four blocks from the array when the slice arguments change
 
   useEffect(() => {
     setCarouselData(
-      [...allBlocksData].slice(sliceStartIndex, sliceStartIndex + MAX_IMAGES)
+      [...allBlocksData].slice(
+        sliceStartIndex,
+        sliceStartIndex + MAX_DISPLAYED_BLOCKS
+      )
     )
   }, [sliceStartIndex, allBlocksData])
 
   const isNextDisabled =
-    sliceStartIndex + MAX_IMAGES > allBlocksData.length ||
+    sliceStartIndex + MAX_DISPLAYED_BLOCKS > allBlocksData.length ||
     allBlocksData.length <= 4
 
-  useEffect(async () => {
-    const carouselData = await getCarouselData()
-    setAllBlocksData(carouselData)
-    if (carouselData) {
-      setIsLoading(false)
+  useEffect(() => {
+    async function fetchData() {
+      const carouselData = await getCarouselData()
+      setAllBlocksData(carouselData)
+
+      if (carouselData) {
+        setIsLoading(false)
+      }
     }
+
+    fetchData()
   }, [])
 
   return (
@@ -49,8 +58,8 @@ function App() {
       {isLoading ? (
         <Loading />
       ) : (
-        <div>
-          <div className='p-8 flex gap-4'>
+        <div className='bg-gray-200 h-screen'>
+          <div className='p-8 flex  gap-4 '>
             {carouselData.map((card, index) => (
               <Card card={card} key={index} />
             ))}

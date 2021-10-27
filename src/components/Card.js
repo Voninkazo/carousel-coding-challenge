7890a3ad5ea9c5e2380535f1e2abe474e32e7579
sandy <sandy.rav@onja.org>
2021-10-27 15:57:10 +0300

    Make the card look more responsive on mobile

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,15 +2,15 @@ import React from 'react'
 
 export function Card({ card }) {
   return (
-    <div className='shadow-contentCardShadow px-4 py-6'>
+    <div className='shadow-contentCardShadow px-4 py-6 whitespace-nowrap'>
       <h2 className='font-bold text-gray-900 text-lg leading-8 pb-4'>
         {card.title}
       </h2>
-      <div>
+      <div className='w-full'>
         <img
-          src={card.images[Math.floor(Math.random() * card.images.length)]}
+          src={card.images[Math.floor(Math.random() * card.images.length)]} // display an image at a time
           alt={card.title}
-          className='sm:w-40 sm:h-40 md:w-80 lg:h-80'
+          className='w-full'
         />
       </div>
     </div>

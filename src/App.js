fe5d4aaffa583471ec793759dc17f0ece6a61eb3
sandy <sandy.rav@onja.org>
2021-10-27 03:37:00 +0300

    Add interfaces, styles and basic functionalities

--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import data from './data.json'
 
 function App() {
+  const [sliceStartIndex, setSliceStartIndex] = useState(0)
+  const [sliceEndIndex, setSliceEndIndex] = useState(4)
+  const [carouselData, setCarouselData] = useState(
+    data.slice(sliceStartIndex, sliceEndIndex)
+  )
+  const [previousData, setPreviousData] = useState()
+
+  function slideToLeft() {}
+
+  function slideToRight() {
+    setSliceStartIndex(sliceStartIndex + 4)
+    setSliceEndIndex(sliceEndIndex + 4)
+  }
+
+  useEffect(() => {
+    setCarouselData([...data].slice(sliceStartIndex, sliceEndIndex))
+  }, [sliceEndIndex])
+
   return (
-    <div>
-      {data.map((card, index) => {
-        return (
-          <div key={index}>
-            <h2>{card.title}</h2>
-            <div>
-              <img src={card.image} alt={card.title} />
+    <>
+      <div className='p-8 flex gap-4'>
+        {carouselData.map((card, index) => {
+          return (
+            <div key={index}>
+              <h2 className='font-bold text-gray-900 text-lg leading-8'>
+                {card.title}
+              </h2>
+              <div>
+                <img src={card.image} alt={card.title} className='w-80 h-80' />
+              </div>
             </div>
-          </div>
-        )
-      })}
-    </div>
+          )
+        })}
+      </div>
+      <div className='flex justify-center gap-6 '>
+        <button onClick={slideToLeft} className='px-8 py-4 bg-black text-white'>
+          Previous
+        </button>
+        <button
+          onClick={slideToRight}
+          className='px-8 py-4 bg-black text-white'>
+          Next
+        </button>
+      </div>
+    </>
   )
 }
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 export function Card({ title, img, className }) {
   return (
-    <div className={`card ${className} `}>
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
+    <div className='bg-white p-4'>
+      <div className={`card ${className} `}>
+        <img src={img} alt={title} />
+        <h2>{title}</h2>
+      </div>
     </div>
   )
 }

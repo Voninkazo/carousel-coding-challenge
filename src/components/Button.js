f4c1eea117e17e0339ebbb88f440182e93c58738
sandy <sandy.rav@onja.org>
2021-10-27 15:13:05 +0300

    Add disabled button styles

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,16 @@
 import React from 'react'
+// cn is a package that combines classnames together
+import cn from 'classnames'
 
 export function Button({ onClick, isDisabled, buttonText }) {
   return (
     <button
       onClick={onClick}
       disabled={isDisabled}
-      className='px-8 py-4 bg-black text-white'>
+      className={cn(
+        'px-8 py-4 bg-black text-white hover:bg-red',
+        `${isDisabled && 'cursor-not-allowed bg-gray-400 '}`
+      )}>
       {buttonText}
     </button>
   )

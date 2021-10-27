8fbf82b5eeb2dbd85f047854e6b9c394a889b5f0
sandy <sandy.rav@onja.org>
2021-10-27 11:49:32 +0300

    Create a loading component

---
+++ src/components/Loading.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function Loading() {
+  return (
+    <div className='relative'>
+      <p className='absolute top-2/4 left-2/4'>Loading...</p>
+    </div>
+  )
+}

fd5e63f5a7ad83d4025fd0e6ae7944aa33481f3c
sandy <sandy.rav@onja.org>
2021-10-27 09:56:25 +0300

    Add styled to each card component and extend the value of tailwind config

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
-export function Card({ title, img, className }) {
+export function Card({ card }) {
   return (
-    <div className='bg-white p-4'>
-      <div className={`card ${className} `}>
-        <img src={img} alt={title} />
-        <h2>{title}</h2>
+    <div className='shadow-contentCardShadow px-4 py-6'>
+      <h2 className='font-bold text-gray-900 text-lg leading-8 pb-4'>
+        {card.title}
+      </h2>
+      <div>
+        <img
+          src={card.image[Math.floor(Math.random() * card.image.length)]}
+          alt={card.title}
+          className='w-80 h-80'
+        />
       </div>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        contentCardShadow: '0px 8px 20px rgba(0, 0, 0, 0.15)',
+      },
+    },
   },
   variants: {
     extend: {},

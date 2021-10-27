dd9897afa64c5bb96e0603aa41d501e7695678ae
sandy <sandy.rav@onja.org>
2021-10-27 15:56:35 +0300

    Modify styles

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export function Loading() {
   return (
-    <div className='flex h-screen justify-center items-center bg-gray-400'>
+    <div className='flex h-screen justify-center items-center'>
       <p className='text-center text-white text-xl font-semibold'>Loading...</p>
     </div>
   )

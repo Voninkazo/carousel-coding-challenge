a81bb1dfa4568d5dd28c09fcc157102d784d3446
sandy <sandy.rav@onja.org>
2021-10-28 15:17:40 +0300

    Add id attribute and styles

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,13 +2,14 @@ import React from 'react'
 // cn is a package that combines classnames together
 import cn from 'classnames'
 
-export function Button({ onClick, isDisabled, buttonText }) {
+export function Button({ onClick, isDisabled, buttonText, id }) {
   return (
     <button
+      id={id}
       onClick={onClick}
       disabled={isDisabled}
       className={cn(
-        'px-8 py-4 bg-black text-white',
+        'px-8 py-4 bg-black text-white rounded',
         `${isDisabled && 'cursor-not-allowed bg-gray-400 '}`
       )}>
       {buttonText}

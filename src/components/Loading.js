e0ecb0dc538ec399d62d14e51d495c3f0711fc30
sandy <sandy.rav@onja.org>
2021-10-28 15:18:14 +0300

    Change loading text color

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,7 +3,7 @@ import React from 'react'
 export function Loading() {
   return (
     <div className='flex h-screen justify-center items-center'>
-      <p className='text-center text-white text-xl font-semibold'>Loading...</p>
+      <p className='text-center text-black text-xl font-semibold'>Loading...</p>
     </div>
   )
 }
